080d5db08097e9100cb37e2b083bc71f7bfd4e9a
ANDY ZHANG <andyzhang@live.ca>
2024-05-16 14:20:45 -0400

    added Apollo tracking

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -4,6 +4,7 @@ export const metadata = {
     "AI assistant for in-house legal; automated matter intake, document management, and much more.",
 }
 
+import ApolloScriptLoader from "@/components/ApolloScriptLoader"
 import Features from "@/components/features"
 import Hero from "@/components/hero"
 import WaitList from "@/components/waitlist"
@@ -11,6 +12,8 @@ import WaitList from "@/components/waitlist"
 export default function Home() {
   return (
     <>
+      <ApolloScriptLoader />
+
       <Hero />
       <Features />
       {/* <FeaturesBlocks /> */}
